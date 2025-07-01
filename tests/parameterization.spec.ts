291118e1f9be0ee27ccc19b90ea21af0dc0339f5
Nadezhda0490 <nadjastrelchenok@gmail.com>
2025-07-01 11:27:55 +0300

    Removed .env from repo and added to .gitignore. Removed extra empty line

--- tests/parameterization.spec.ts
+++ tests/parameterization.spec.ts
@@ -1,10 +1,9 @@
 import { test, expect } from "@playwright/test";
 import { userData } from "./test-data/user-data";
 
-test.describe("Page availability", async () => {
+test.describe.only("Page availability", async () => {
   for (const { testPage, pageKey, isLogout } of userData) {
     test(`should load ${testPage} and display site logo`, async ({ page }) => {
-        
         if(isLogout) {
             await page.goto('/inventory.html');
             await page.click('.bm-burger-button')
